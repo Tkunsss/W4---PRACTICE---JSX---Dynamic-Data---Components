90a84fb59cef728254b88f32cf7c21283da2a7fa
Tkunsss <sotikun6@gmail.com>
2026-02-05 11:23:18 +0700

    first commit

---
+++ Start code/exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Foother from "./components/foother";
+function App() {
+  return (
+    <>
+    <Header />
+    <Main />
+    <Foother />
+    <p>hello</p>
+</>)
+}
+
+export default App
